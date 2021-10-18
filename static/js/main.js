9efd7e2353f919fa1d38686c8588b3a96c42d660
ungureanudaniel <ungureanu.daniel86@gmail.com>
2021-10-18 22:57:56 +0300

    Style updates

--- static/js/main.js
+++ static/js/main.js
@@ -1,11 +1,3 @@
-/*  ---------------------------------------------------
-    Template Name: Foodeiblog
-    Description:  Foodeiblog Blog HTML Template
-    Author: Colorlib
-    Author URI: https://colorlib.com
-    Version: 1.0
-    Created: Colorlib
----------------------------------------------------------  */
 
 'use strict';
 
