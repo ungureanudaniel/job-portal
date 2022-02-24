b1377f583394aa59e2f07f06cfa2b11454ad5ea7
ungureanudaniel <ungureanu.daniel86@gmail.com>
2022-02-24 13:15:15 +0200

    Re designed the ad div with a background vid and a map of EU, fixed header carousel to hide on small screens

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
-from .views import user_login, register, contact, about, account, privacy, terms, registration_conf_view, registration_success_view
+from .views import user_login, register, contact, about, account, privacy, terms, registration_conf_view, registration_success_view, maintenance
+
 
 urlpatterns = [
     path('login/', user_login, name='login'),
@@ -15,5 +17,6 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('privacy-policy/', privacy, name='privacy-policy'),
     path('terms-of-service/', terms, name='terms-of-service'),
+    ]
 
-]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/views.py
+++ users/views.py
@@ -11,6 +11,12 @@ from django.contrib.auth.models import User
 from django.views.decorators.csrf import csrf_protect
 import random
 
+def maintenance(request):
+    template = 'users/maintenance.html'
+
+    context = {}
+    return render(request, template, context)
+
 def login(request):
     return render(request, 'users/login.html')
 #-------------------------------LOGIN VIEW-----------------------------------
